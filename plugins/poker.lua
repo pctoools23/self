cd7fd757cedeba27c68e3cc9fdfdd343266938a1
roboplus <alizolfaghary@outlook.com>
2016-04-08 00:03:24 -0700

    Update poker.lua

--- plugins/poker.lua
+++ plugins/poker.lua
@@ -2,8 +2,8 @@ do
 
 function run(msg, matches)
 local reply_id = msg['id']
-local text = '😐'
-if matches[1] == '😐' then
+local text = '😐''😉''😳'😒'😁''😊'😕'😞'😆'😋'😀'
+if matches[1] == '😐''😉''😳'😒'😁''😊'😕'😞'😆'😋' then
     if not is_sudo(msg) then
 reply_msg(reply_id, text, ok_cb, false)
 end
@@ -12,6 +12,13 @@ end
 return {
 patterns = {
     "😐"
+    "😉"
+    "😳"
+    "😁"
+    "😕"
+    "😞"
+    "😆"
+    "😋"
 },
 run = run
 }
